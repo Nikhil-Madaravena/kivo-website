b9de7dfbfb53181777bf7d669ec6f7f818867b53
Nikhil Madaravena <nikhil.madaravena@gmail.com>
2026-05-02 00:53:13 +0530

    chore: make deployment ready for github pages

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Kivo-InMemory-Database">
+    <BrowserRouter basename="/kivo-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
